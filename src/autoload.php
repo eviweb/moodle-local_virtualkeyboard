55aad62a6aef0d18d9a66c95ec70c991f8c68acc
Eric Villard <dev@eviweb.fr>
2015-03-13 17:09:46 +0100

    add class autoloader
    Signed-off-by:Eric Villard <dev@eviweb.fr>

---
+++ src/autoload.php
@@ -0,0 +1,49 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * The MIT License
+ *
+ * Copyright 2015 Eric VILLARD <dev@eviweb.fr>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package     local_virtualkeyboard
+ * @author      Eric VILLARD <dev@eviweb.fr>
+ * @copyright   (c) 2015 Eric VILLARD <dev@eviweb.fr>
+ * @license     http://opensource.org/licenses/MIT MIT License
+ */
+
+if (!function_exists('local_virtualkeyboard_autoload')) {
+    function local_virtualkeyboard_autoload($class)
+    {
+        $file = __DIR__.DIRECTORY_SEPARATOR.
+            str_replace('\\', DIRECTORY_SEPARATOR, $class).
+            '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return true;
+        }
+        return false;
+    }
+
+    if (!in_array('local_virtualkeyboard_autoload', spl_autoload_functions())) {
+        spl_autoload_register('local_virtualkeyboard_autoload');
+    }
+}
