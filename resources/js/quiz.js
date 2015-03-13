948d1ba883fb1dfb21b70ba222740560fc724012
Eric Villard <dev@eviweb.fr>
2015-03-13 10:20:21 +0100

    fix keyboard image uri + force french keyboard
    Signed-off-by:Eric Villard <dev@eviweb.fr>

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -1,1 +1,6 @@
-Y.all('input[id$=answer][type=text]').addClass('keyboardInput');
+Y.all('input[id$=answer][type=text]').each(function(inputNode) {
+    inputNode.set('lang', 'fr');
+    VKI_attach(inputNode.getDOMNode());    
+});
+
+Y.all('img.keyboardInputInitiator').setAttribute('src', '/local/virtualkeyboard/resources/img/keyboard.png');
