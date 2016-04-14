d9ece117399bf46f5a5e36456f96c1cf1e6c97ab
Eric Villard <dev@eviweb.fr>
2016-04-14 16:29:04 +0200

    fix keyboard never shown in embedded questions (cloze)
    force css display value for the keyboard table
    
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -3,6 +3,14 @@ function enableVirtualKeyboardForQuiz(Y, lang) {
         inputNode.set('lang', lang || 'en');
         VKI_attach(inputNode.getDOMNode());    
     });
+    
+    // fix issue with embedded questions (cloze): keyboard never shown
+    // force css display value for the keyboard table
+    Y.all('input[id$=answer][id*=sub][type=text]').each(function(inputNode) {
+        inputNode.next('img.keyboardInputInitiator').on('click', function(e) {
+            Y.one('table[id=keyboardInputMaster]').setStyle('display', '');
+        });  
+    });
 
     Y.all('img.keyboardInputInitiator')
         .setAttribute('src', '/local/virtualkeyboard/resources/img/keyboard.png');
